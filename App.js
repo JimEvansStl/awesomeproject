9ec8fc8129fb4eea5213410e73376bfe83642527
Jim Evans <iago.evans@gmail.com>
2024-04-24 20:01:46 -0500

    Component colors

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ColorBox from './components/ColorBox';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Here are some boxes of difference colors</Text> 
+      <ColorBox colorName="Cyan" hexCode="#2aa198"/> 
+      <ColorBox colorName="Blue" hexCode="#268bd2"/> 
+      <ColorBox colorName="Magenta" hexCode="#d33682"/> 
+      <ColorBox colorName="Orange" hexCode="#cb4b16"/> 
       <StatusBar style="auto" />
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 10,
+    paddingTop: 40,
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
   },
 });
