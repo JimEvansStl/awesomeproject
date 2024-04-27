6d4271c6a51cba0d4dee16d6b883d357724e4593
Jim Evans <iago.evans@gmail.com>
2024-04-27 17:18:34 -0500

    here

--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
 
-const Home = () => {
+const Home = ({ navigation }) => {
   return (
     <View>
-      <Text>Solarized</Text>
+      <TouchableOpacity onPress={() => navigation.navigate("ColorPalette")}>
+        <Text>Solarized</Text>
+      </TouchableOpacity>
     </View>
   );
 };
